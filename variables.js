1353ec4e0022f0a3fddac721debf63745b4110c4
popovichkristian <popovichkristian@gmail.com>
2022-08-12 18:19:29 +0300

    variables

---
+++ variables.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let number = 5;
+const leftBoredrWidth = 1;
+
+number = 10;
+console.log(number);
+ 
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+console.log(obj);
+
+{
+    let result = 50;
+};
+

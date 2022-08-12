6b0da377464a29bca671da545131439a7979e3ec
popovichkristian <popovichkristian@gmail.com>
2022-08-12 23:57:28 +0300

    Interpolation (ES6)

---
+++ Interpolation (ES6).js
@@ -0,0 +1,6 @@
+// 008 Интерполяция (ES6)
+const category = 'toys';
+console.log(`https://someurl.com/${category/5}`); // ``- при использовании косых кавычек...
+
+const user = 'Ivan';
+alert(`Привет, ${user}`);
